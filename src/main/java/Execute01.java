1343e7aa9577a3fa6a2b36be425b97b01b5c2d1f
Enes Batur <enay10171@gmail.com>
2023-10-16 23:26:56 +0300

    JDBC v1.0

---
+++ src/main/java/Execute01.java
@@ -0,0 +1,43 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1.ADIM : Driver göster
+        Class.forName("org.postgresql.Driver"); //Java 7 ile birlikte buna gerek kalmadı
+
+        //2.ADIM : Bağlantıyı oluşturma
+        Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db","techpront","password");
+
+        //3. ADIM : Statement nesnesini oluşturma : SQL sorgularını DB ye gönderme ve çalıştırma
+        Statement st=con.createStatement();
+
+        //4. ADIM : sorgu oluşturma ve çalıştırma
+
+
+        //ÖRNEK 1:"workers" adında bir tablo oluşturup "worker_id,worker_name,salary" sütunlarını ekleyiniz.
+        boolean sql1=st.execute("CREATE TABLE IF NOT EXISTS workers(worker_id INT, worker_name VARCHAR(50),salary REAL)");
+
+        //DQL için kullanılırsa : ResultSet nesnesi alınırsa TRUE değerini döndürür.
+        //DDL için kullanılırsa : ResultSet nesnesi almadığı için FALSE döndürür.
+
+        System.out.println("sql1 = " + sql1);
+
+        //ÖRNEK 2:"workers" tablosuna VARCHAR(20) tipinde "city" sütununu ekleyiniz.
+        boolean sql2=st.execute("ALTER TABLE workers ADD COLUMN IF NOT EXISTS city VARCHAR(20)");
+        System.out.println("sql2 = " + sql2);
+
+        //ÖRNEK 3:"workers" tablosunu SCHEMAdan siliniz.
+        boolean sql3 = st.execute("DROP TABLE IF EXISTS workers");
+        System.out.println("sql3 = " + sql3);
+
+
+        //5.ADIM : Bağlantıyı ve statementı kapatma
+        st.close();
+        con.close();
+
+    }
+}
---
+++ src/main/java/ExecuteQuery01.java
@@ -0,0 +1,63 @@
+import java.sql.*;
+
+public class ExecuteQuery01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db","techpront","password");
+        Statement st = con.createStatement();
+
+        //ÖRNEK 1:id'si 5 ile 10 arasında olan ülkelerin "country_name" bilgisini listeleyiniz.
+
+        ///kayıtlarımızı görmek için executeQuery() kullanmalıyız.
+        ResultSet resultSet =st.executeQuery("SELECT country_name FROM countries WHERE id BETWEEN 5 AND 10");
+//        resultSet.next();
+//        System.out.println("Ülke ismi = " + resultSet.getString("country_name"));
+        while(resultSet.next()){
+            System.out.println("Ülke ismi = " + resultSet.getString("country_name"));
+//            String name= resultSet.getString("country_name");
+//            List<String> names=new ArrayList<>();
+//            names.add(resultSet.getString("country_name"));
+        }
+
+        //ResultSet'te geriye dönüş yoktur.
+
+        //ÖRNEK 2: phone_code'u 600 den büyük olan ülkelerin "phone_code" ve "country_name" bilgisini listeleyiniz.
+
+        ResultSet rs1=st.executeQuery("SELECT phone_code, country_name FROM countries WHERE phone_code >600");
+        while (rs1.next()){
+            System.out.println("tel kodu: "+rs1.getInt("phone_code")+" - "+rs1.getString("country_name"));
+        }
+
+        //ÖRNEK 3:it_persons tablosunda "salary" değeri en düşük salary olan yazılımcıların tüm bilgilerini gösteriniz.
+
+        ResultSet rs2=st.executeQuery("SELECT * FROM it_persons WHERE salary = (SELECT MIN(salary) FROM it_persons)");
+
+        while (rs2.next()){
+            System.out.println(rs2.getInt("id")+"--"+rs2.getString("name")+"--"
+                    +rs2.getDouble("salary")+"--"+rs2.getString("prog_lang"));
+        }
+
+
+        //ÖRNEK 4:Puanı bölümlerin taban puanlarının ortalamasından yüksek olan öğrencilerin isim ve puanlarını listeleyiniz.ÖDEVV
+
+
+        ResultSet rs3= st.executeQuery("SELECT isim,puan FROM ogrenciler WHERE puan> (SELECT AVG(taban_puani) FROM bolumler)");
+
+        while (rs3.next()){
+            System.out.println("Isim : "+ rs3.getString("isim")+" Puan : "+ rs3.getInt("puan") );
+        }
+
+
+
+
+
+
+
+
+
+
+        st.close();
+        con.close();
+
+    }
+}
